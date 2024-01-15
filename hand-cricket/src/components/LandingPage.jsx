4deb7e1cd0118d7bca6b9ea3ba2a51189976c2fd
Rakesh Mandal <rkm8448746347@gmail.com>
2024-01-16 00:20:52 +0530

    utils need to be added

---
+++ hand-cricket/src/components/LandingPage.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import {useNavigate} from "react-router-dom"
+
+const LandingPage = () => {
+    let [checked, setChecked] = useState(false);
+    let NavigateTo = useNavigate();
+
+    let RouteToToss = () => {
+        if (!checked) {
+            alert("please select the checkbox first!"); 
+            return;
+        }
+        NavigateTo("/Toss")
+    }
+  return (
+    <div
+      style={{
+        minHeight: "88vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContetent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          padding: "0.5em",
+          margin: "0.5em",
+        }}
+      >
+        <p
+          style={{
+            color: "pink",
+            fontSize: "1.2em",
+            marginBottom: "1em",
+            textDecoration: "underline",
+          }}
+        >
+          Rules To Play:
+        </p>
+        <ul style={{ marginBottom: "1em" }}>
+          <li>Its a hand-cricket match game of one over each side. </li>
+          <li>
+            There will be 3 rounds in Total and whoever wins the most matches
+            will won the game.{" "}
+          </li>
+          <li>
+            Before each round, there will be a toss to decide who is going to
+            bat first.
+          </li>
+          <li>
+            The player batting first will pick a number between 1-6 for each
+            ball of an over and bowler will try to guess the number and if he
+            guessed it correctly the batsman will be out and then the bowler
+            will bat and try to score more than the batsman to win the match.{" "}
+          </li>
+          <li>
+            similar to this round , there will be 4 more rounds to decide the
+            ultimate winner the game{" "}
+          </li>
+          <li>
+            After finishing all 3 rounds, it could also possible that you and
+            the other player has won equal matches because of some draw matches
+            in some rounds, so you can re-play the game to decide the winner
+          </li>
+        </ul>
+        <input onClick={(e) => setChecked(e.target.checked)} type="checkbox" />{" "}
+        <span style={{ color: "white", marginLeft: "0.4em" }}>
+          Have you read all the details and rules of the game{" "}
+        </span>
+      </div>
+
+      <button
+        onClick={RouteToToss}
+        className="Btn"
+        style={{
+          background: "goldenRod",
+          marginTop: "8vh",
+           boxShadow: "1px 1px 0.3em 0.1em pink" ,fontStyle:"italic"
+        }}
+      >
+        Proceed To Play
+      </button>
+    </div>
+  );
+}
+
+export default LandingPage
---
+++ hand-cricket/src/components/Recommedation.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+let recommendations = [
+  "/fUvylznXMQc?si=ypoBhfGMQFRDZsn5",
+  "/XpoiGVBWQ08?si=0mbIbf0yg5srtjZv",
+  "/hK6Z-ooqqVc?si=nyje0aMjloM_Y_Ik",
+  "/Pk_qBTqTLs0?si=QcdJLxlL660VOdCV",
+  "/32PouIfy02Y?si=_M3JYqNMTnM7xVm1",
+  "/pgGJGrcAhIU?si=bJ87H1-T5LlePoPf",
+  "/WmVVWf6oBF8?si=vud6G9_dchoJytVX",
+  "/Pk_qBTqTLs0?si=YnX12tet833IVCdy",
+];
+
+const Recommedation = () => {
+
+  return (
+      <div className=" mt-5 p-2 d-flex  bg-info overflow-x-scroll">
+          
+          {recommendations.map((videoId, i) =>
+            <iframe
+              key={i}
+                  className="m-2 rounded "
+                  height="200"
+                  src={`https://www.youtube.com/embed${videoId} `}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>)}
+      
+    </div>
+  );
+}
+
+export default Recommedation
